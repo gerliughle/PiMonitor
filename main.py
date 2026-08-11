2cf2ce34dd72fda0a6eeaf8304761982a5b40345
Josh Shaw <joshua.shaw3@pcc.edu>
2026-08-10 18:25:48 -0700

    testing CPU temp

---
+++ main.py
@@ -0,0 +1,11 @@
+def get_cpu_temp():
+    """ Reads Pi CPU temp in C """
+    try:
+        with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+            temp_raw = int(f.read().strip())
+            return round(temp_raw / 1000.0, 2)
+    except FileNotFoundError:
+        print("Thermal zone file not found.")
+        return 0.0
+
+print(f"Current CPU temp: {get_cpu_temp()}°C")
